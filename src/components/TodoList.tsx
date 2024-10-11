b967ec0728f7d6b619a4317cffedf7e58f35f524
kennzuki <kingsyken@gmail.com>
2024-10-11 15:44:56 +0300

    todo list ui component done

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,13 @@
-
+import { FaTrashAlt } from "react-icons/fa";
+import { TiTick } from "react-icons/ti";
 
 const TodoList = () => {
   return (
-      <div>
+      <div className="flex place-items-center justify-between border p-4 shadow-xl rounded-xl">
           <h1 className="">todo</h1>
-          <section className="">
-              <button className="">finished</button>
-              <button className="">delete</button>
+          <section className="flex place-items-center gap-3 [&_button]:px-3 [&_button]:border [&_button]:rounded-xl [&_button]:py-2 [&_button]:shadow-md [&_button]:text-white [&_button]:flex [&_button]:place-items-center [&_button]:gap-2">
+              <button className="bg-green-500"><TiTick />finished</button>
+              <button className="bg-red-500"><FaTrashAlt />delete</button>
           </section>
     </div>
   )
